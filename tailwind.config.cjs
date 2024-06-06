4d1a7cc5d4d6f1c60fc2230db70c0ac0485e96d1
Ciel <cielcpark@gmail.com>
2024-06-06 14:07:26 -0400

    nav, hero, and bg

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,7 +1,22 @@
+
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			keyframes: {
+				animatedgradient: {
+				  '0%': { backgroundPosition: '0% 50%' },
+				  '50%': { backgroundPosition: '100% 50%' },
+				  '100%': { backgroundPosition: '0% 50%' },
+				},
+			  },
+			  backgroundSize: {
+				'300%': '300%',
+			  },
+			  animation: {
+				gradient: 'animatedgradient 10s ease infinite alternate',
+			  },
+		},
 	},
 	plugins: [],
 };
