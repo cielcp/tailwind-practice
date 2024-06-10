a913d0ee9fc909445de9c600edc19c8263575ea8
Ciel <cielcpark@gmail.com>
2024-06-10 16:47:56 -0400

    Prettified with Prettier

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,22 +1,21 @@
-
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {
-			keyframes: {
-				animatedgradient: {
-				  '0%': { backgroundPosition: '0% 50%' },
-				  '50%': { backgroundPosition: '100% 50%' },
-				  '100%': { backgroundPosition: '0% 50%' },
-				},
-			  },
-			  backgroundSize: {
-				'300%': '300%',
-			  },
-			  animation: {
-				gradient: 'animatedgradient 10s ease infinite alternate',
-			  },
-		},
-	},
-	plugins: [],
-};
+    content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+    theme: {
+        extend: {
+            keyframes: {
+                animatedgradient: {
+                    '0%': { backgroundPosition: '0% 50%' },
+                    '50%': { backgroundPosition: '100% 50%' },
+                    '100%': { backgroundPosition: '0% 50%' },
+                },
+            },
+            backgroundSize: {
+                '300%': '300%',
+            },
+            animation: {
+                gradient: 'animatedgradient 10s ease infinite alternate',
+            },
+        },
+    },
+    plugins: [],
+}
